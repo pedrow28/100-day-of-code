d2c70b7f59487c93d6c157b5cd6c7292e2b1c382
Pedro William <pedrowilliamrd@gmail.com>
2022-09-30 13:56:01 -0300

    Arrumando branches

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -1,5 +1,24 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+# Window
+
+window = Tk()
+window.title("Gerenciador de Senhas")
+window.config(padx=20, pady=20)
+
+
+# Logo
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=2, row=1)
+
+
+
+window.mainloop()
